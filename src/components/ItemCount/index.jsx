f253c97ef9fff59545d078f04d3edfdd554e225c
fran17sk <francis_cruz2@hotmail.com.ar>
2022-07-28 13:10:37 -0300

    'desafioItemCount'

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const ItemCount = ( {initial , stock , onAdd} ) =>{
+    const [count,setCount]=useState(initial);
+
+    const increment = () => count < stock && setCount(count+1);
+    const decrement = () => count > initial && setCount(count-1);
+
+
+    return (
+        <div className="CardProd">
+        <div className="ItemCount">
+            <button onClick={decrement} className='buttonItemCount'>-</button>
+            <p className="Count">{count}</p>
+            <button onClick={increment} className='buttonItemCount'>+</button>
+        </div>
+        <div>
+            <p className="textStock">stock:{stock}</p>
+        </div>
+        <div>
+            <button className="buttonAddToCart">AGREGAR AL CARRITO</button>
+        </div>
+        </div>
+    )
+}
+
+export { ItemCount }
--- src/components/ItemListContent.js
+++ src/components/ItemListContent.js
@@ -1,6 +1,9 @@
+import { ItemCount } from './ItemCount';
+
 const ItemListContent = (props) => {
     return (<>
         <p>{props.greeting}</p>
+        <ItemCount initial={1} stock={100} onAdd={()=>{}}/>
     </>)
 }
 
